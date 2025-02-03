17e621d5e1387d52eae72fbc4ff62e37f72454fc
Roloya28 <choej9742@naver.com>
2025-02-03 21:44:53 +0900

    중간 세이브

--- schedule.sql
+++ schedule.sql
@@ -5,8 +5,8 @@ use calendar;
 create table calendar (
                           id int auto_increment primary key,
                           author varchar(100) not null COMMENT '작성자',
-                          todolist varchar(255) not null COMMENT '스케쥴',
-                          password varchar(100) not null COMMENT '비밀번호',
+                          todolist varchar(100) not null COMMENT '스케쥴',
+                          password varchar(50) not null COMMENT '비밀번호',
                           createDate timestamp default current_timestamp,
                           updateDate timestamp default current_timestamp on update current_timestamp
 );
